fe18798c4ecfc3a85cf0aa278b5eb068271dd13e
Artemdjdj <artembasuraa@gmail.com>
2026-02-10 14:36:39 +0300

    [feat] create number descriptor

---
+++ lab_1/src/utils/descriptor.py
@@ -0,0 +1,23 @@
+from typing import Any
+
+
+class NumberValidator:
+    def __init__(
+        self, min_value: int | float = None, max_value: int | float = None
+    ) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def __set_name__(self, owner: Any, name: str) -> None:
+        self.name = f"__{name}"
+
+    def __get__(self, instance: Any, owner: Any) -> int | float:
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance: Any, value: int | float) -> None:
+        if isinstance(value, (int|float)):
+            raise TypeError("The value is not int and not float")
+        elif value < self.min_value:
+            raise ValueError(f"The value is too small, min value  is {self.min_value}")
+        elif value > self.max_value:
+            raise ValueError(f"The value is too big, max value  is {self.max_value}")
