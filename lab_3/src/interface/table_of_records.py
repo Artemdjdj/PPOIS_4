7bfd737bdfa0d8ab7ed5b984cb5f7e0c7cf734dd
Artemdjdj <artembasuraa@gmail.com>
2026-03-03 22:59:34 +0300

    [feat] create list of leaders in table records

--- lab_3/src/interface/table_of_records.py
+++ lab_3/src/interface/table_of_records.py
@@ -7,7 +7,8 @@ from src.interface.button import Button
 from src.interface.mixins.additional_menu import AdditionalMenuMixin
 from src.settings.settings import SCREEN_WIDTH, BUTTON_WIDTH, COORD_Y_BACK_BUTTON, BUTTON_HEIGHT, BASIC_COLOR, \
     BASIC_BACKGROUND_COLOR, BASIC_BACKGROUND_HOVER_COLOR, NAME_BACK, BASIC_FONT, BASIC_FONT_SIZE, \
-    TABLE_RECORDS_BACKGROUND_IMAGE, SCREEN_HEIGHT
+    TABLE_RECORDS_BACKGROUND_IMAGE, SCREEN_HEIGHT, TEST_LEADERS, COORD_Y_START_TABLE_RECORDS, RULES_FONT_SIZE, \
+    RULES_COLOR, COORD_X_RECORD, COORD_X_NAME, COORD_X_NUMBER
 from src.settings.state import State
 
 
@@ -16,4 +17,17 @@ class TableRecords(AdditionalMenuMixin):
         super().__init__(screen, TABLE_RECORDS_BACKGROUND_IMAGE)
 
     def _write_leaders(self) -> None:
-        pass
+        leaders = TEST_LEADERS.copy()
+        y_pos = COORD_Y_START_TABLE_RECORDS
+        for leader in leaders:
+            text_number = pygame.font.Font(BASIC_FONT, RULES_FONT_SIZE).render(str(leader["number"]), True, RULES_COLOR)
+            text_name = pygame.font.Font(BASIC_FONT, RULES_FONT_SIZE).render(str(leader["name"]), True, RULES_COLOR)
+            text_record = pygame.font.Font(BASIC_FONT, RULES_FONT_SIZE).render(str(leader["record"]), True, RULES_COLOR)
+            self._screen.blit(text_number, (COORD_X_NUMBER, y_pos))
+            self._screen.blit(text_name, (COORD_X_NAME, y_pos))
+            self._screen.blit(text_record, (COORD_X_RECORD, y_pos))
+            y_pos += text_number.get_height()
+
+    def draw(self)->None:
+        super().draw()
+        self._write_leaders()
--- lab_3/src/settings/settings.py
+++ lab_3/src/settings/settings.py
@@ -17,10 +17,10 @@ BASIC_FONT = BASE_DIR / "assets/fonts/main.otf"
 BASIC_FONT_SIZE = 20
 RULES_FONT_SIZE = 18
 
-BASIC_BACKGROUND_COLOR = (128,0,0)
-BASIC_BACKGROUND_HOVER_COLOR=(200,0,0)
-BASIC_COLOR = (255,255,255)
-RULES_COLOR = (0,0,0)
+BASIC_BACKGROUND_COLOR = (128, 0, 0)
+BASIC_BACKGROUND_HOVER_COLOR = (200, 0, 0)
+BASIC_COLOR = (255, 255, 255)
+RULES_COLOR = (0, 0, 0)
 
 NAME_PLAY = "Играть"
 NAME_TABLE_RECORD = "Таблица рекордов"
@@ -28,13 +28,45 @@ NAME_HELP = "Справка"
 NAME_QUIT = "Выйти"
 NAME_BACK = "Назад"
 
-COORD_Y_FIRST_BUTTON = SCREEN_HEIGHT*0.2375
-COORD_Y_START_TEXT = SCREEN_HEIGHT*0.20
-COORD_Y_BACK_BUTTON = SCREEN_HEIGHT*0.60
-HEIGHT_BETWEEN_BUTTONS = SCREEN_HEIGHT*0.1125
+COORD_Y_FIRST_BUTTON = SCREEN_HEIGHT * 0.2375
+COORD_Y_START_TEXT = SCREEN_HEIGHT * 0.20
+COORD_Y_START_TABLE_RECORDS = SCREEN_HEIGHT * 0.25
+COORD_Y_BACK_BUTTON = SCREEN_HEIGHT * 0.60
+COORD_X_NUMBER = SCREEN_WIDTH * 0.25
+COORD_X_NAME = SCREEN_WIDTH * 0.37
+COORD_X_RECORD = SCREEN_WIDTH * 0.70
+HEIGHT_BETWEEN_BUTTONS = SCREEN_HEIGHT * 0.1125
 
 MENU_BACKGROUND_IMAGE = BASE_DIR / "assets/images/result_menu2.png"
 HELP_MENU_BACKGROUND_IMAGE = BASE_DIR / "assets/images/help_menu.png"
 TABLE_RECORDS_BACKGROUND_IMAGE = BASE_DIR / "assets/images/table_records.png"
 
 FILE_RULES_PATH = BASE_DIR / "assets/config/rules.txt"
+
+TEST_LEADERS = [
+    {
+        "number": 1,
+        "name": "Artemdjdj",
+        "record": 100
+    },
+    {
+        "number": 2,
+        "name": "zhkulik",
+        "record": 85
+    },
+    {
+        "number": 3,
+        "name": "ionhavetimee",
+        "record": 80
+    },
+    {
+        "number": 4,
+        "name": "dburbas",
+        "record": 70
+    },
+    {
+        "number": 5,
+        "name": "Shoptick",
+        "record": 60
+    }
+]
