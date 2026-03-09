3c63978f14a60e67057d4926c721fd6e5462113b
Artemdjdj <artembasuraa@gmail.com>
2026-03-09 21:53:33 +0300

    [feat] new logic of destroying toilet

--- lab_3/src/objects/toilet.py
+++ lab_3/src/objects/toilet.py
@@ -1,6 +1,6 @@
 import pygame
 
-from src.settings.settings import CAR_IMAGE, TOILET_IMAGE, DESTROYED_TOILET_IMAGE, TOILET_SHOOT
+from src.settings.settings import CAR_IMAGE, TOILET_IMAGE, DESTROYED_TOILET_IMAGE, TOILET_SHOOT, NOT_TOILET_IMAGE
 from src.objects.base_sprite import BaseSprite, BaseObjectInLayer
 
 
@@ -8,16 +8,32 @@ class Toilet(BaseObjectInLayer):
     def __init__(self, x:int, y:int, layer_speed:float) -> None:
         self._image_toilet = pygame.image.load(TOILET_IMAGE).convert_alpha()
         self._image_destroyed_toilet = pygame.image.load(DESTROYED_TOILET_IMAGE).convert_alpha()
+        self._image_not_toilet = pygame.image.load(NOT_TOILET_IMAGE).convert_alpha()
+        self._count_of_shoots = 0
+        self._is_toilet_nothing = False
         super().__init__(self._image_toilet, x, y, layer_speed)
 
+    def shoot(self)->None:
+        self._count_of_shoots += 1
+
+        if self._count_of_shoots >3:
+            self._is_killed = True
+
+        if self._count_of_shoots>9:
+            self._is_toilet_nothing = True
+
     def animate(self) -> None:
-        if self._is_killed:
-            x,y = self.rect.x, self.rect.y
-            self.image = self._image_destroyed_toilet
+        if self._is_killed or self._image_not_toilet:
+            x, y = self.rect.x, self.rect.y
+            if self._is_killed and not self._is_toilet_nothing:
+                self.image = self._image_destroyed_toilet
+            elif self._is_toilet_nothing:
+                self.image = self._image_not_toilet
             self.rect = self.image.get_rect()
             self.rect.x = x
             self.rect.y = y
 
     def play(self, volume:float) -> None:
-        self._player.set_sound(TOILET_SHOOT)
-        super().play(volume)
+        if not self._is_toilet_nothing:
+            self._player.set_sound(TOILET_SHOOT)
+            super().play(volume)
