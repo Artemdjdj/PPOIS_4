a8873206cb4b7feea140ad0d39b1cb10aa02b8d0
Artemdjdj <artembasuraa@gmail.com>
2026-03-26 09:27:33 +0300

    [feat] add app tools

--- lab_4/garden_plot/garden_plot/urls.py
+++ lab_4/garden_plot/garden_plot/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('plants/', include("plants.urls")),
+    path('tools/', include("tools.urls")),
 ]
