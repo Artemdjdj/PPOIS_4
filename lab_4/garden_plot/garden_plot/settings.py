9ea56b0b8069318c3243d0928b9f8ba453ceca1a
Artemdjdj <artembasuraa@gmail.com>
2026-03-26 08:59:03 +0300

    [feat] add app main

--- lab_4/garden_plot/garden_plot/settings.py
+++ lab_4/garden_plot/garden_plot/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -114,4 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
